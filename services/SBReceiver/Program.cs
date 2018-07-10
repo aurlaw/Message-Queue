0ab84b149fa70522a25944d62a7a75ee296868e4
Michael Lawrence <mlawrence@gatesmandave.com>
2018-07-10 16:25:21 -0400

    init

---
+++ services/SBReceiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SBReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Receiver");
+        }
+    }
+}
---
+++ services/SBSender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SBSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From Sender");
+        }
+    }
+}
