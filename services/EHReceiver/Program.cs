aab56cc045b83bf90adb7e41921490e2a86ffc93
Michael Lawrence <mlawrence@gatesmandave.com>
2018-07-11 17:09:02 -0400

    initial projects for EventHub

---
+++ services/EHReceiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EHReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
