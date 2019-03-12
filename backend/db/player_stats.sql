04880a3567006d6a06add37234b30bd43a812a41
Michael Lawrence <mlawrence@gatesmandave.com>
2019-03-12 12:30:03 -0400

    removed unrelated projects

--- backend/db/player_stats.sql
+++
@@ -1,6 +0,0 @@
-SELECT s.Name, *
-  FROM [dbo].[SkaterStatistics] sk 
-  inner join [dbo].[Seasons] s ON s.Id = sk.SeasonId
-  where sk.PlayerId = 1
-  AND s.Name Like '%Regular%'
-  order by s.Name
--- services/GraphQL.Api/clientApp/src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Footer.css';
-
-export default props => (
-    <footer>
-        Last Updated: 09.02.2018
-    </footer>
-
-)
